14c5babd1565bc0e196c2dcb9054b6b0cf3e49c7
SiahBatterson <siahbatterson@gmail.com>
2024-11-15 09:03:08 -0500

    d

--- javascript_game/js/terraingeneration.js
+++ javascript_game/js/terraingeneration.js
@@ -60,6 +60,9 @@ export function generateRandomMap(rows, cols, config = {}) {
       if (map[y][x] === "#" && map[y + 1][x] === "%") {
         map[y][x] = "%"; // Changed grass to ground
       }
+      if (map[y][x] === "%" && map[y + 1][x] === ".") {
+        map[y][x] = "#"; // Remove stacked grass tile
+      }
 
       previousRowHasTop[x] = map[y][x] === "#";
 
